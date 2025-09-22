d704718fa445f4a31a21668f82aac8b8d8c6b0d8
deeksha puri <deekshapuri15@gmail.com>
2025-09-22 15:15:04 +0530

    Add files via upload

---
+++ 3Q5 b.c
@@ -0,0 +1,20 @@
+   #include <stdio.h>
+    int main(){
+	
+    int a;
+    printf("enter the number",a);
+    scanf(" %d", &a);
+    (a %2==0) ?
+	printf("even no")
+	:
+	printf("odd no");
+	 
+    
+    return 0;
+    }    
+
+
+	
+	
+
+    
