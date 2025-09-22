89acb0a13354a1177b453aa7cc922c85f40bac37
deeksha puri <deekshapuri15@gmail.com>
2025-09-22 15:32:08 +0530

    Add files via upload

---
+++ 3Q6.c
@@ -0,0 +1,25 @@
+   #include <stdio.h>
+    int main(){
+	
+    int a;
+    printf("enter the number ");
+    scanf("%d", &a); 
+	if ( a %5==0 && a%8==0){
+    printf(" divisible \n");
+	}
+	else 
+	{ 
+	printf("not divisible\n");
+	}
+    
+    
+	 
+    
+    return 0;
+    }    
+
+
+	
+	
+
+    
