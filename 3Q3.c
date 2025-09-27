910992e087253da47e6b49431fbae07f03b13a31
deeksha puri <deekshapuri15@gmail.com>
2025-09-28 00:26:18 +0530

    Add files via upload

---
+++ 3Q3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter an operator (+, -, *, /, %): ");
+    scanf(" %c", &op);   // space before %c important hai
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    switch(op) {
+        case '+':
+            printf("Result = %d\n", a + b);
+            break;
+        case '-':
+            printf("Result = %d\n", a - b);
+            break;
+        case '*':
+            printf("Result = %d\n", a * b);
+            break;
+        case '/':
+            if (b != 0)
+                printf("Result = %d\n", a / b);
+            else
+                printf("Error: Division by zero!\n");
+            break;
+        case '%':
+            if (b != 0)
+                printf("Result = %d\n", a % b);
+            else
+                printf("Error: Modulo by zero!\n");
+            break;
+        default:
+            printf("Invalid operator!\n");
+    }
+
+    return 0;
+}
+
