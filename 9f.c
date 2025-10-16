099c161a9715cd11035b257c011b1d0d5e7e98f1
deeksha puri <deekshapuri15@gmail.com>
2025-10-16 10:28:49 +0530

    Add files via upload

---
+++ 9f.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+	  int num,i;
+	  printf("enter a no",num);
+	  scanf("%d",&num);
+	  
+	  for(i=1;i<=10;i++){
+      printf(" %d * %d= %d",num,i,num*i);
+	  }
+	  
+	  return 0;
+	  
+	} 
+
